4208b589bc11416f7d81d99dc8dd12528ca88626
pgoel05 <prateekgoyal29@gmail.com>
2020-07-25 00:56:39 +0530

    Week 4

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -37,16 +37,17 @@ grad = zeros(size(theta));
 %
 
 
+h = sigmoid(X * theta);
 
+J = (1/m) * ((-y' * log(h))-((1-y)' * log(1-h))) + (lambda/(2*m)) * (theta(2:length(theta)))' * (theta(2:length(theta)));
 
+temp_theta = theta;
+temp_theta(1) = 0;
 
-
-
-
+grad = ((1/m) * (h-y)' * X) + (lambda/m) * temp_theta';
 
 
 % =============================================================
-
 grad = grad(:);
 
 end
